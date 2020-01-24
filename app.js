cd88ca99f72760bb88751284260c94c544f0ed0e
entertthk <markkc2@hotmail.com>
2020-01-24 10:29:43 +0200

    js

---
+++ app.js
@@ -0,0 +1,31 @@
+//listen to the click event on the get jokes button
+
+//select the button, add the event listener
+console.log("test");
+
+document.querySelector('.get-jokes').addEventListener('click', getJokes);  
+
+function getJokes(event){
+   const userNumber = document.querySelector('input[type="number"]').value;
+
+   //GET request
+   fetch(`http://api.icndb.com/jokes/random/${userNumber}`)
+   .then(function(response){
+       console.log(response);
+       return response.json();
+   })
+   .then(function(data){
+        let output = '';
+        console.log(data.value);
+
+        //for each joke in data.value array
+        data.value.forEach(joke =>{
+            output +=`<li>${joke.joke}</li>`;
+            console.log(joke.joke);
+        });
+
+        document.querySelector('.jokes').innerHTML = output;
+   });
+
+    event.preventDefault();
+}
